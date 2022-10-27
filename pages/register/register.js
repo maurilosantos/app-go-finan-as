33360c1cebe3b0a5d707d3c7be41b769c0688ea8
Maurilo Santos <109925535+maurilosantos@users.noreply.github.com>
2022-10-27 11:20:51 -0300

    second commit

--- pages/register/register.js
+++ pages/register/register.js
@@ -1,10 +1,3 @@
-firebase.auth().onAuthStateChanged(user => {
-    if (user) {
-        window.location.href = "../home/home.html";
-    }
-})
-
-
 function onChangeEmail(){
     const email = form.email().value;
     form.emailRequiredError().style.display = email ? "none" : "block";
@@ -31,25 +24,10 @@ function onChangeConfirmPassword(){
 function register(){
     showLoading();
 
-    const email = form.email().value;
-    const password = form.password().value;
-    firebase.auth().createUserWithEmailAndPassword(
-        email, password
-    ).then( ()=> {
-        hideLoading();
         window.location.href = "../../pages/home/home.html";
-    }).catch(error => {
+   
         hideLoading();
-        alert(getErrorMessage(error));
-    })
-}
-
-function getErrorMessage(error) {
-    if (error.code == "auth/email-already-in-use"){
-        return "Email já está em uso :)"
     }
-    return error.message;
-}
 
 function validatePasswordsMatch(){
     const password = form.password().value;
