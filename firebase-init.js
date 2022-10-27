839c1dc0f95b97a0ef813d7423dd7ef2a345ad70
Maurilo Santos <109925535+maurilosantos@users.noreply.github.com>
2022-10-27 10:58:41 -0300

    second commit

--- firebase-init.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyA3JbkomSkZEBcvGUbpYFdU8OtZBcKpn0c",
-    authDomain: "finance-app-93c6a.firebaseapp.com",
-    projectId: "finance-app-93c6a",
-    storageBucket: "finance-app-93c6a.appspot.com",
-    messagingSenderId: "903048992135",
-    appId: "1:903048992135:web:774db87d932d385362af22"
-  };
-    firebase.initializeApp(firebaseConfig);
